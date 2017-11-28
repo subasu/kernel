b0bb8b9c83887810c23c0326fd496508c1f7ac68
subasu <arashshir70@gmail.com>
2017-11-28 09:05:42 +0330

    some small changes

--- app/Http/SelfClasses/AddCategory.php
+++ app/Http/SelfClasses/AddCategory.php
@@ -10,6 +10,7 @@ class AddCategory
     //
     public function addNewCategory($category)
     {
+        print_r($category);
         $checkDbNotToBeNull = DB::table('categories')->first();
         $counter = 0;
         if(count($checkDbNotToBeNull) > 0)
@@ -18,14 +19,18 @@ class AddCategory
             $counter += $latestCategory->id;
         }
         $count = count($category);
+       // dd($count);
         $i = 0;
+        $j = $count-1;
         while($i < $count)
         {
             $insert = DB::table('categories')->insert
             ([
-                 'title' => $category[$i]
+                 'title' => $category[$i],
+                 'depth' => $j
             ]);
             $i++;
+            $j--;
         }
         $newCategories = DB::table('categories')->select('id')->where('id', '>' ,$counter)->get();
         $len=count($newCategories);
@@ -46,7 +51,7 @@ class AddCategory
             print('اطلاعات با موفقیت ثبت گردید ، لطفا جهت ثبت دسته های جدید ابتدا دسته های موجود را بررسی نمایید  در صورت عدم وجود دسته مورد نظر دکمه ثبت دسته  جدید را بزنید');
         }else
             {
-                print('در ثبت اطلاعات خطایی رخ داده است ، لطفا با بخش پشتیبانی تماس بگیرید.');
+                print('در ثبت اطلاعات خطایی رخ داده است ، لطفا با بخش پشتیبانی تماس بگیرید');
             }
 
     }
