ca8a47661d0b38c030c1b443e8e0477d9bf06f87
mitikoman2017-9 <shervinpardaz2016@gmail.com>
2017-11-29 08:41:48 +0330

    8-9-96

---
+++ app/Http/SelfClasses/addProduct.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artan
+ * Date: 11/29/2017
+ * Time: 8:26 AM
+ */
+
+namespace App\Http\SelfClasses;
+
+
+class addProduct
+{
+    public function addProduct()
+    {
+
+    }
+}
